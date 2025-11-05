22fcb778bea29409591c393a6ab6bee9524fffd5
Chinmay Shet <chishxd@gmail.com>
2025-11-05 21:49:24 +0530

    feat: Design Map structure and render walls

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use bracket_lib::prelude::*;
 
+#[derive(Clone, Copy, PartialEq)]
+
+enum TileType {
+    Wall,
+    Floor,
+}
+
+struct Map {
+    tiles: Vec<TileType>,
+}
+
+impl Map {
+    fn xy_to_index(x: i32, y: i32) -> usize {
+        (y as usize * 80) + x as usize
+    }
+}
+
 struct State {
+    map: Map,
     player_x: i32,
     player_y: i32,
 }
@@ -10,6 +28,23 @@ impl GameState for State {
         self.player_input(ctx);
 
         ctx.cls();
+
+        let mut y = 0;
+
+        for tile in self.map.tiles.iter() {
+            let x = y % 80;
+            let py = y / 80;
+
+            match tile {
+                TileType::Floor => {
+                    ctx.set(x, py, GHOSTWHITE, BLACK, to_cp437(' '));
+                }
+                TileType::Wall => {
+                    ctx.set(x, py, RED, BLACK, to_cp437('#'));
+                }
+            }
+            y += 1;
+        }
 
         ctx.set(self.player_x, self.player_y, WHITE, BLACK, to_cp437('@'));
     }
@@ -35,9 +70,23 @@ fn main() -> BError {
         .with_title("The Sonar Maze")
         .build()?;
 
+    let mut new_map = Map {
+        tiles: vec![TileType::Floor; 80 * 50],
+    };
+
+    for x in 0..80 {
+        new_map.tiles[Map::xy_to_index(x, 0)] = TileType::Wall;
+        new_map.tiles[Map::xy_to_index(x, 49)] = TileType::Wall;
+    }
+    for y in 0..50 {
+        new_map.tiles[Map::xy_to_index(0, y)] = TileType::Wall;
+        new_map.tiles[Map::xy_to_index(79, y)] = TileType::Wall;
+    }
+
     main_loop(
         context,
         State {
+            map: new_map,
             player_x: 40,
             player_y: 25,
         },
