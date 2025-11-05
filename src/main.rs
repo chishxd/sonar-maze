91b050c77d73fc1f13357e9385b9a4c8c489f16d
Chinmay Shet <chishxd@gmail.com>
2025-11-05 21:11:42 +0530

    feat: Player moves now

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,26 @@ struct State {
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
+        self.player_input(ctx);
+
         ctx.cls();
 
         ctx.set(self.player_x, self.player_y, WHITE, BLACK, to_cp437('@'));
+    }
+}
+
+impl State {
+    fn player_input(&mut self, ctx: &mut BTerm) {
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::Left => self.player_x -= 1,
+                VirtualKeyCode::Right => self.player_x += 1,
+                VirtualKeyCode::Up => self.player_y -= 1,
+                VirtualKeyCode::Down => self.player_y += 1,
+
+                _ => {}
+            }
+        }
     }
 }
 
