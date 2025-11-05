7fdb6da46d7ed38f10d7f2d4711cdc4e28ea2866
Chinmay Shet <chishxd@gmail.com>
2025-11-05 20:50:00 +0530

    feat: Create Main Window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, World!");
+use bracket_lib::prelude::*;
+
+struct State {
+    player_x: i32,
+    player_y: i32,
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+
+        ctx.set(self.player_x, self.player_y, WHITE, BLACK, to_cp437('@'));
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("The Sonar Maze")
+        .build()?;
+
+    main_loop(
+        context,
+        State {
+            player_x: 40,
+            player_y: 25,
+        },
+    )
 }
